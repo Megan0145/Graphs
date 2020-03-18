fa324b7854ad5938158e24559bd86c4370b87ee7
Megan Ennis <Megan@Megans-iMac-9.local>
2020-03-18 12:11:03 +0000

    Add function to populate graph with users and friendships

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,3 +1,5 @@
+import random
+
 class User:
     def __init__(self, name):
         self.name = name
@@ -45,9 +47,34 @@ class SocialGraph:
         # !!!! IMPLEMENT ME
 
         # Add users
-
+        # loop in range 0 -> number of users..
+        for i in range(num_users):
+            # add new user on each iteration, their name will be an f-string with the current index within loop concatenated
+            self.add_user(f'User {i}')
         # Create friendships
+        # declare list to hold all possible friendship combinations
+        possible_friendship_combos = []
+        # iterate over users..
+        for user_id in self.users:
+            # iterate over users again in a range from user id + 1 to last id + 1...
+            for friend_id in range(user_id + 1, self.last_id + 1):
+                # add tuple to possible_friendship_combos list containing the user id and friend id
+                possible_friendship_combos.append((user_id, friend_id))
+        
+        # shuffle the possible_friendship_combos to randomise it
+        random.shuffle(possible_friendship_combos)
 
+        # the number of friendships to generate can be calculated by multiplying the number of users by the average number of 
+        # friendships per user and floor dividing in half (because friendships are biderectional -> ie each time add_friendship() 
+        # called it creates two friendships)
+        num_friendships_to_gen = num_users * avg_friendships // 2
+
+        # loop in range num_friendships_to_generate...
+        for i in range(num_friendships_to_gen):
+            # add friendship for tuple at current index of possible_friendship_combos
+            self.add_friendship(possible_friendship_combos[i][0], possible_friendship_combos[i][1])
+
+            
     def get_all_social_paths(self, user_id):
         """
         Takes a user's user_id as an argument
